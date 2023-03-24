142ce1205e0343b56a17b92047db78fc721781a8
David Bucher <d.bucher97@gmail.com>
2023-03-24 11:22:08 +0100

    remote path updates

---
+++ lambda_env.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env sh
+
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/lib/python3/dist-packages/tensorflow/"
+export LIBRARY_PATH="$LIBRARY_PATH:/usr/lib/python3/dist-packages/tensorflow/"
